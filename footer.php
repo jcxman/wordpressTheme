8783c20eb9b37337ff917db4e0af15a782967cd3
chenxinji <695881071@qq.com>
2016-01-30 18:58:14 -0500

    update new theme demo, wait for more with angularjs

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
 <footer class="site-footer"><p><?php bloginfo('name')?> - &copy; <?php echo date("m/d/y") ?></p>
+    <nav class="site-nav">
+        <?php
+
+        $args = array(
+            'theme_location' =>'primary'
+        );
+
+        ?>
+        <?php wp_nav_menu($args); ?>
+    </nav>
 </footer>
 </div>
 <?php wp_footer(); ?>
